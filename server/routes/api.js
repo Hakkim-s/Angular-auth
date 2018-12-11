95009394bb484e7fd7b2b7da5ea2b742184c6580
krithikakl <jenifer.a@optisolbusiness.com>
2018-12-11 18:34:29 +0530

    setting up mongoose connections

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+const db = "mongodb://userhakkim:Keepbboying@3@ds261969.mlab.com:61969/eventdb"
+
+mongoose.connect(db, err =>{
+  if(err){
+    console.error('Error!' + err)
+  } else{
+    console.log('Connected to mongodb')
+  }
+})
 router.get('/',(req, res) => {
   res.send('From API route')
 })
